788d73e1d96102248c81b9d29a5567c508cf17ae
Zachary Smith <info@zachis.it>
2018-01-03 17:41:33 -0500

    v0.0.4 - now plays sound files on android

--- platforms/browser/www/js/main.js
+++ platforms/browser/www/js/main.js
@@ -12,9 +12,15 @@ $(document).ready(function() {
 var audioClip;
 
 function audioFiles() {
+    var localPath = '/sounds/';
+    var p = window.location.pathname;
+    var root = p.substring(0, p.lastIndexOf('/')) + localPath;
+
+    //window.alert(p);
+    //window.alert(root);
     var files = [
-        "../sounds/ScruvyRat.mp3",
-        "../sounds/test.mp3"
+        root + "ScruvyRat.mp3",
+        root + "test.mp3"
     ];
 
     var randomAudio = files[Math.floor(Math.random()*files.length)];
@@ -23,19 +29,47 @@ function audioFiles() {
 }
 
 function playSound() {
-    $('#make_sound').on('click', (function() {
+    $('body').click(function(evt){
+        if(!$(evt.target).is('#make_sound')) {
+            if(audioClip) audioClip.stop();
+        } else {
+            //event handling code
+            if (Media.MEDIA_RUNNING) {
+                if(audioClip) audioClip.stop();
+                audioClip = new Media(audioFiles());
+                //audioClip = new Media(audioFiles(), null, onError);
+            }
 
+            /*function onError(error) {
+             alert('code: '    + error.code    + '\n' +
+             'message: ' + error.message + '\n');
+             }*/
+            //$(this).toggleClass('playing');
+            audioClip.play();
+        }
+    });
+
+    /*$('#make_sound').on('click', (function() {
+
+        //window.alert(window.location.pathname);
         //var what = $(this);
         //showToggleImage(what);
+
+        //window.alert(audioFiles());
 
         if (Media.MEDIA_RUNNING) {
             if(audioClip) audioClip.stop();
             audioClip = new Media(audioFiles());
+            //audioClip = new Media(audioFiles(), null, onError);
         }
 
+        /*function onError(error) {
+            alert('code: '    + error.code    + '\n' +
+                'message: ' + error.message + '\n');
+        }*/
         //$(this).toggleClass('playing');
-        audioClip.play();
-    }));
+        //audioClip.play();
+    /*}));*/
 }
 
 function showToggleImage(what) {
